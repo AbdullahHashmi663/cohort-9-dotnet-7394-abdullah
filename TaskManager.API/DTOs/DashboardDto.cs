fb8389ca8e4cc4b41ba6da496dbcb02fbdb5df65
Abdullah Bin Zubair Hashmi <abdullahbinzubairhashmi@gmail.com>
2026-09-11 19:53:43 +0500

    fix: add missing dashboard properties to DashboardDto

--- TaskManager.API/DTOs/DashboardDto.cs
+++ TaskManager.API/DTOs/DashboardDto.cs
@@ -6,5 +6,15 @@ namespace TaskManager.API.DTOs
         public int InProgressCount { get; set; }
         public int CompletedCount { get; set; }
         public int TotalCount { get; set; }
+
+        public int TotalTasks { get => TotalCount; set => TotalCount = value; }
+        public int PendingTasks { get => PendingCount; set => PendingCount = value; }
+        public int InProgressTasks { get => InProgressCount; set => InProgressCount = value; }
+        public int CompletedTasks { get => CompletedCount; set => CompletedCount = value; }
+
+        public int HighPriorityTasks { get; set; }
+        public int MediumPriorityTasks { get; set; }
+        public int LowPriorityTasks { get; set; }
+        public List<TaskResponseDto> RecentTasks { get; set; } = new();
     }
 }
